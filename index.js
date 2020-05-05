1a77db08cc4221f0b2ef83cc657eed68197714e6
Hunter Stevens <hunterstevens1996@gmail.com>
2020-05-05 14:58:10 -0600

    C:/Program Files/Git/api/post get request working

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const postsRouter = require('./postsRouter');
 
 server.use(express.json());
 
+server.get('/', (req, res) => {
+    res.status(200).json({
+        api:'Welcome to posts!'
+    })
+});
 
+
+server.use('/api/posts', postsRouter);
 
 server.listen(8000, () => console.log('Node-API2 project up and running!'));
--- postsRouter.js
+++ postsRouter.js
@@ -1,3 +1,39 @@
 const express = require('express');
+const db = require('./data/db');
 
 const router = express.Router();
+
+router.get('/', (req,res) =>{
+    db.find(req.query)
+    .then(article =>{
+        res.status(200).json(article);
+    })
+    .catch(err =>{
+        console.log(err);
+        res.json(500).json({message:"The posts info could not be retrieved."});
+    })
+})
+
+router.get('/:id/comments', (req,res) =>{
+    
+})
+
+router.post('/', (req,res) =>{
+    db.insert(req.body)
+    .then(article =>{
+        res.status(200).json()
+    })
+})
+
+router.post('/:id/comments', (req,res) =>{
+    
+})
+
+router.delete('/:id', (req,res) =>{
+    
+})
+
+router.put('/:id', (req,res) =>{
+    
+})
+module.exports = router;
