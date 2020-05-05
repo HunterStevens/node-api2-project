28957011813e1a975db4220d0dd77ca354797315
Hunter Stevens <hunterstevens1996@gmail.com>
2020-05-05 15:43:46 -0600

    add error messages and function to the api/posts/:id GET request

--- postsRouter.js
+++ postsRouter.js
@@ -15,7 +15,16 @@ router.get('/', (req,res) =>{
 })
 
 router.get('/:id', (req,res) =>{
-    
+    db.findById(req.params.id)
+    .then(article =>{
+        if(article.length === 0){
+            res.status(404).json({ message: "The post with the specified ID does not exist." })
+        }
+        else{res.status(200).json(article);}
+    })
+    .catch(err =>{
+        res.status(500).json({ error: "The post information could not be retrieved." });
+    })
 })
 
 router.get('/:id/comments', (req,res) =>{
